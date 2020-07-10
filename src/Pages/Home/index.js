714161ab0d2e4f5cf84d5cc8b99805f028053fe8
haywill20 <mirandacaceresm6@gmail.com>
2020-07-10 01:00:09 -0600

    primer commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,37 +1,46 @@
-import React from 'react'
-import NavBar from '../../components/NavBar'
-import Categories from '../../components/Categories'
-import Carousel from '../../components/Carousel'
-import InterestingProducts from '../../components/InterestingProducts'
-import FeaturedProducts from '../../components/FeaturedProducts'
-import Footer from '../../components/Footer'
-import AboveNavBar from "../../components/AboveNavBar";
-import SubNavBar from "../../components/SubNavBar";
-import './index.css'
+import React from "react";
 
+import Navbar1 from "../../components/navbar1/index.js";
+import Navbar2 from "../../components/navbar2/index.js";
+import Navbar3 from "../../components/navbar3/index.js";
+import Navbar4 from "../../components/navbar4/index.js";
+import Categorias from "../../components/Categories/index.js";
+import Carousel from "../../components/Carousel/index.js";
+import Seccion3 from "../../components/seccion3/index.js";
+import Slider1 from "../../components/slider1/index.js";
+import Slider2 from "../../components/slider2/index.js";
+import Slider3 from "../../components/slider3/index.js";
+import Informacion from "../../components/informacion/index.js";
+import Footer from "../../components/Footer/index.js";
 export default function Home() {
   return (
     <>
-        <AboveNavBar />
-        <NavBar />
-        <SubNavBar />
-        <div className="container-fluid px-4">
-          <div className="row mt-4 px-5">
-            <div className="col-xl-3">
-              <Categories />
-            </div>
-            <div className="col-xl-9 ">
-              <Carousel />
-            </div>
+      <Navbar1 />
+      <Navbar2 />
+      <Navbar3 />
+      <Navbar4 />
+      <div className="container-fluid px-4">
+        <div className="row mt-4 px-5">
+          <div className="col-xl-3">
+            <Categorias />
+          </div>
+          <div className="col-xl-6">
+            <Carousel />
+          </div>
+          <div className="col-xl-3">
+            <Seccion3 />
           </div>
         </div>
-        
-        <div className="products-container container-fluid px-4">
-          <InterestingProducts />
-          <FeaturedProducts />
-        </div>
+        <Slider1 />
+        <br></br>
+        <Slider2 />
+        <br></br>
+        <Slider3 />
+        <br></br>
+        <Informacion />
+      </div>
 
-        <Footer />
+      <Footer />
     </>
-  )
+  );
 }
--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,37 +1,129 @@
-import React, { Component } from 'react'
-import {FiChevronRight} from 'react-icons/fi'
-import './index.css'
+import React, { Component } from "react";
+import "./index.css";
 
+import { FiChevronRight } from "react-icons/fi";
 const icons = {
-  fontSize: 14
-} 
-
-export default class Categories extends Component {
-  
+  fontSize: 14,
+};
+export default class Categorias extends Component {
   render() {
-    return(
+    return (
       <>
         <div className="categories">
           <span className="text-uppercase"> Categorias </span>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Accesorios </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Teléfonos celulares y accesorios </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Computadoras, Tabletas y Oficina </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Electrónica de consumo </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Drones, Juguetes y Pasatiempos </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Salud y cuidado personal </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Mejoras para el hogar y herramientas </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Hogar & Jardín </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Industrial y científica </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Electrónica de motores y automóviles </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Aire libre, fitness y deportes </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Relojes y Joyas </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Zapatos y ropa </a> </li>
-          <li className="item-categories"> <a className="link-categories" href="/404">  <FiChevronRight size={icons.fontSize}/> Promoción Gearbest </a> </li>
-          
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Accesorios{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Teléfonos celulares{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Computadoras, Tabletas y
+              Oficina{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Electrónica de consumo{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Drones, Juguetes y
+              Pasatiempos{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Comida rapida platillos y
+              más{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Zapatos, Ropa hombre y
+              mujer{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Limpieza de interiores{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Industrial y científica{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Electrónica de motores y
+              automóviles{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Aire libre, fitness y
+              deportes{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Relojes y Joyas{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Zapatos y ropa{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Articulos de ferreteria{" "}
+            </a>{" "}
+          </li>
+          <li className="item-categories">
+            {" "}
+            <a className="link-categories" href="/404">
+              {" "}
+              <FiChevronRight size={icons.fontSize} /> Mascotas{" "}
+            </a>{" "}
+          </li>
         </div>
       </>
-    )
+    );
   }
-
-} 
-
+}
